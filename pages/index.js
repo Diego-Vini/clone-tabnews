fda4f9a01d6e081a57f399718af312a18b199460
Diego Vinicius Matheus Martins <analistamm@gmail.com>
2025-09-09 08:59:31 +0000

    modificando a frase

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home(){
     return <h1>Aprender algo novo é desafiador, há momentos 
         de duvida, questionamentos e até tristeza, porém, entender os
-        motivos que o fizeram começar e persistir tentando trará
+        motivos que o fizeram começar e persistir trará
         também pequenos momentos de vitórias, que aos poucos se tornarão
         maiores que as duvidas, questinamentos e tristezas.
         Quando se der conta, entenderá o quanto valeu a pena continuar.
