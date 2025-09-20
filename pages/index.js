7c5140e502b2bf9df372dde5261ea7cca3fd59fe
Diego Vinicius Matheus Martins <analistamm@gmail.com>
2025-09-20 20:17:26 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
-function Home(){
-    return <h1>Aprender algo novo é desafiador, há momentos 
-        de duvida, questionamentos e até tristeza, porém, entender os
-        motivos que o fizeram começar e persistir trará
-        também pequenos momentos de vitórias, que aos poucos se tornarão
-        maiores que as duvidas, questinamentos e tristezas.
-        Quando se der conta, entenderá o quanto valeu a pena continuar.
+function Home() {
+  return (
+    <h1>
+      Aprender algo novo é desafiador, há momentos de duvida, questionamentos e
+      até tristeza, porém, entender os motivos que o fizeram começar e persistir
+      trará também pequenos momentos de vitórias, que aos poucos se tornarão
+      maiores que as duvidas, questinamentos e tristezas. Quando se der conta,
+      entenderá o quanto valeu a pena continuar.
     </h1>
+  );
 }
 
-export default Home; 
+export default Home;
